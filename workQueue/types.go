48aa0750436d92f041265b70a3fe34c44f2f2af3
luo ying <411426150@qq.com>
2019-10-22 10:56:09 +0800

    Update types.go

--- workQueue/types.go
+++ workQueue/types.go
@@ -1,7 +1,7 @@
 package workQueue
 
 const (
-	mqUrl     = "amqp://admin:admin123@192.168.99.214:5672/"
+	mqUrl     = "amqp://admin:admin123@127.0.0.1:5672/"
 	queueName = "push.msg.q"
 	exchange  = "t.msg.ex"
 )
